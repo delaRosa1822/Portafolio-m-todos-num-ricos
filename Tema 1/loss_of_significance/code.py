7ac34e0c8ca70c1f44d7dfca428082e74edef304
delaRosa1822 <24030247@itesa.edu.mx>
2026-04-30 21:54:13 -0600

    Add code to demonstrate loss of significance

---
+++ Tema 1/loss_of_significance/code.py
@@ -0,0 +1,10 @@
+# Dos números muy grandes y casi iguales
+x = 1.000000000000002
+y = 1.000000000000001
+
+# Matemáticamente la resta debería ser 0.000000000000001
+resultado = x - y
+
+# Mostramos el resultado en consola
+print("Resultado de la resta:")
+print(resultado)
