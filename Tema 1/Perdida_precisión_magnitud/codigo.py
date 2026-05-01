93835fcb187e110add4a3397174cbcd3fe7c9c97
delaRosa1822 <24030247@itesa.edu.mx>
2026-04-30 21:51:08 -0600

    Add code to demonstrate precision loss in floating point

---
+++ Tema 1/Perdida_precisión_magnitud/codigo.py
@@ -0,0 +1,13 @@
+# Un 1 con 16 ceros (magnitud gigante)
+a = 10000000000000000.0
+
+# Un número pequeño
+b = 0.1
+
+# Realizamos la suma
+resultado = a + b
+
+# Mostramos los resultados en consola
+print(f"Valor de A: {a}")
+print(f"Valor de B: {b}")
+print(f"Suma (A + B): {resultado}")
