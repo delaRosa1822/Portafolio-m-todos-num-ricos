9ebaca5bed3a3ec925289bcd3b533559d8d40af0
delaRosa1822 <24030247@itesa.edu.mx>
2026-04-30 21:38:15 -0600

    Add comparison example in Comparacion.py

---
+++ Tema 1/Comparacion_directa/Comparacion.py
@@ -0,0 +1,11 @@
+# Realizamos la resta
+resultado = 1.1 - 1.0
+
+# Mostramos el resultado real guardado en memoria
+print(f"Resultado de la resta: {resultado}")
+
+# Comparamos directamente con ==
+if resultado == 0.1:
+    print("Es exactamente 0.1")
+else:
+    print("No es 0.1!")
