6158a11eaab0a2ada06e3c155bc181bb8ef277b4
delaRosa1822 <24030247@itesa.edu.mx>
2026-04-30 21:44:04 -0600

    Add example of binary rounding error in Python

---
+++ Tema 1/Error de redondeo binario/codigo.py
@@ -0,0 +1,24 @@
+# Declaramos nuestras variables
+numero1 = 0.1
+numero2 = 0.2
+
+# Realizamos la suma
+resultado = numero1 + numero2
+
+# Mostramos el resultado. ¡Sorpresa! No será 0.3 cerrado.
+print(f"La suma de {numero1} + {numero2} es:")
+print(resultado)
+## Código Fuente
+```python
+# Ejemplo clásico de Error de Redondeo Binario en Python
+
+# Declaramos nuestras variables
+numero1 = 0.1
+numero2 = 0.2
+
+# Realizamos la suma
+resultado = numero1 + numero2
+
+# Mostramos el resultado. ¡Sorpresa! No será 0.3 cerrado.
+print(f"La suma de {numero1} + {numero2} es:")
+print(resultado)
